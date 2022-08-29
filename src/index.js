b5c8c233e2c71a32d61d8bfb3749cdc630c260e2
Crosscut <crosscutcode@gmail.com>
2022-08-29 19:04:41 -0400

    Fixed null issue

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import changeDescription from "./changeDescription";
 import changePriority from "./changePriority";
 import changeTitle from "./changeTitle";
 import createTodo from "./createTodo";
+import createProject from "./createProject";
 import './style.css';
 
+    const projBtn = document.getElementById('projBtn');
+    const taskBtn = document.getElementById('listBtn');
+    const listForm = document.querySelector('.list-form')
+    const projForm = document.querySelector('.project-form');
+    
+    console.log(document);
+    console.log(projBtn);
 
-changeDate();
-changeDelete();
-changeDescription();
-changePriority();
-changeTitle();
-createTodo();
+    
+
+
+
